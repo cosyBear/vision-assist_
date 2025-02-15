330fffdd4b90121c87025a9597d41d52a9bd4b04
teddy <ali.alzamili@student.kdg.be>
2025-02-15 18:10:38 +0100

    the fixation point can not go outSide the screen the text is centered

--- lib/pages/display_page/display_page.dart
+++ lib/pages/display_page/display_page.dart
@@ -6,10 +6,6 @@ import 'package:steady_eye_2/pages/display_page/wigdt/scrolling_text_view.dart';
 import '../../general/app_setting_provider.dart';
 import 'wigdt/draggable_button.dart';
 
-/*
-  This is the main page of the display. It will display the text that was passed to it.
-  It will also contain the focus point that the user can drag around the screen.
- */
 class DisplayPage extends StatefulWidget {
   final String title;
 
@@ -24,26 +20,22 @@ class _DisplayPageState extends State<DisplayPage> {
   late double yPos;
   double textOffset = 0;
 
-  /*
-  This method will be called after the first frame is rendered.
-  This is where we will set the initial position of the button.
-   */
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      double screenWidth = MediaQuery.of(context).size.width;
-      double screenHeight = MediaQuery.of(context).size.height;
+      final screenWidth = MediaQuery.of(context).size.width;
+      final screenHeight = MediaQuery.of(context).size.height;
 
       final settings = Provider.of<AppSettingProvider>(context, listen: false);
-      double buttonSize = settings.fontSize * 2; // Button size is twice the font size
+      final buttonSize = settings.fontSize * 2;
 
       setState(() {
+        // If we already have saved positions, use them. Otherwise center the button.
         if (settings.xPos != 0 && settings.yPos != 0) {
           xPos = settings.xPos;
           yPos = settings.yPos;
         } else {
-          // Center the button if the user has not set a position
           xPos = (screenWidth / 2) - (buttonSize / 2);
           yPos = (screenHeight / 2) - (buttonSize / 2);
           settings.setXPos(xPos);
@@ -56,16 +48,14 @@ class _DisplayPageState extends State<DisplayPage> {
   @override
   Widget build(BuildContext context) {
     final settings = Provider.of<AppSettingProvider>(context, listen: false);
-    double appBarHeight = AppBar().preferredSize.height;
-    double screenHeight = MediaQuery.of(context).size.height;
-    double buttonSize = settings.fontSize; // Button size is twice the font size
+    final buttonSize = settings.fontSize * 2;
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(18, 18, 18, 1.0),
+        backgroundColor: const Color.fromRGBO(18, 18, 18, 1.0),
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.grey),
+          icon: const Icon(Icons.arrow_back, color: Colors.grey),
           onPressed: () => Navigator.of(context).pop(),
         ),
         title: GradientText(
@@ -77,43 +67,72 @@ class _DisplayPageState extends State<DisplayPage> {
           ],
         ),
       ),
-      body: Stack(
-        children: [
-          ScrollingTextView(text: widget.title, textOffset: textOffset),
-          ScrollControls(),
-          DraggableButton(
-              xPos: xPos,
-              yPos: yPos,
-              onPositionChanged: (dx, dy) {
-                setState(() {
-                  xPos += dx;
-                  yPos += dy;
+      body: SafeArea(
+        // Use LayoutBuilder to get the actual available size
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            // These are the *real* width and height of the SafeArea (below AppBar)
+            final availableWidth = constraints.maxWidth;
+            final availableHeight = constraints.maxHeight;
 
-                  if(yPos < appBarHeight) {
-                    yPos = appBarHeight;
-                  }
+            return Stack(
+              children: [
+                // 1) Scrolling text
+                Positioned.fill(
+                  child: ScrollingTextView(
+                    text: widget.title,
+                    textOffset: textOffset,
+                  ),
+                ),
 
-                  if(yPos > screenHeight - buttonSize) {
-                    yPos = screenHeight - buttonSize;
-                  }
-                  settings.setXPos(xPos);
-                  settings.setYPos(yPos);
+                // 2) Scroll controls at bottom-center
+                ScrollControls(),
 
-                  //double screenHeight = MediaQuery.of(context).size.height;
-                  double textMiddleY = screenHeight / 2;
+                // 3) Draggable button
+                Positioned(
+                  left: xPos,
+                  top: yPos,
+                  child: DraggableButton(
+                    xPos: xPos,
+                    yPos: yPos,
+                    onPositionChanged: (dx, dy) {
+                      setState(() {
+                        // Update positions
+                        xPos += dx;
+                        yPos += dy;
 
-                  if (yPos > textMiddleY + 20) {
-                    textOffset = -20;
-                  } else if (yPos < textMiddleY - 20) {
-                    textOffset = 20;
-                  } else {
-                    textOffset = 0;
-                  }
-                });
-              }),
-        ],
+                        // Calculate boundaries based on the actual layout size
+                        final minX = 0.0;
+                        final maxX = availableWidth - buttonSize;
+                        final minY = 0.0;
+                        final maxY = availableHeight - buttonSize;
+
+                        // Clamp to keep the button in view
+                        xPos = xPos.clamp(minX, maxX);
+                        yPos = yPos.clamp(minY, maxY);
+
+                        // Save new positions
+                        settings.setXPos(xPos);
+                        settings.setYPos(yPos);
+
+                        // Adjust text offset based on button position
+                        final textMiddleY = availableHeight / 2;
+                        if (yPos > textMiddleY + 20) {
+                          textOffset = -20;
+                        } else if (yPos < textMiddleY - 20) {
+                          textOffset = 20;
+                        } else {
+                          textOffset = 0;
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
 }
-
--- lib/pages/display_page/wigdt/draggable_button.dart
+++ lib/pages/display_page/wigdt/draggable_button.dart
@@ -4,30 +4,29 @@ import '../../../general/app_setting_provider.dart';
 
 class DraggableButton extends StatelessWidget {
   final double xPos, yPos;
-  final Function(double, double) onPositionChanged;
+  final Function(double dx, double dy) onPositionChanged;
 
   const DraggableButton({
     super.key,
     required this.xPos,
     required this.yPos,
-    required this.onPositionChanged
+    required this.onPositionChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     final settings = Provider.of<AppSettingProvider>(context, listen: false);
-    double buttonSize = settings.fontSize * 2; // Button size is twice the font size
+    final buttonSize = settings.fontSize * 2;
 
-    return Positioned(
-      left: xPos,
-      top: yPos,
-      child: GestureDetector(
-        onPanUpdate: (details) => onPositionChanged(details.delta.dx, details.delta.dy),
-        child:  Image.asset(
-          'assets/images/logo.png',
-          width: buttonSize,
-          height: buttonSize,
-        ),
+    return GestureDetector(
+      onPanUpdate: (details) {
+        // Instead of passing xPos/yPos, pass the delta (dx, dy)
+        onPositionChanged(details.delta.dx, details.delta.dy);
+      },
+      child: Image.asset(
+        'assets/images/logo.png',
+        width: buttonSize,
+        height: buttonSize,
       ),
     );
   }
